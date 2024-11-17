08cc77762df7e474ce8099b5e727db1b4a5c3d99
Rajesh Sharma <36369426+rajeshsharma98@users.noreply.github.com>
2024-11-17 00:56:25 -0500

    Updated - All

--- Qualtrics/app.py
+++ Qualtrics/app.py
@@ -7,4 +7,4 @@ def home():
     return render_template("index.html")  # This will load your main HTML page
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80,debug=True)
