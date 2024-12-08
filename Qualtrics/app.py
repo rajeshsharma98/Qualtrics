ff5c2e44db3cba8112a321c046cbde5c5337a87e
Ruchi-Bhogate <ruchibhogate11@gmail.com>
2024-12-08 17:20:11 -0500

    add jsonify

--- Qualtrics/app.py
+++ Qualtrics/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, jsonify, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 
